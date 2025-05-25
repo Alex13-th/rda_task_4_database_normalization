17a3dc81b4c0231066fb083279866efafa42b534
Alex S. <aleks.shaparenko@gmail.com>
2025-05-25 17:27:55 +0200

    Solution2

--- task.sql
+++ task.sql
@@ -4,19 +4,19 @@ CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE Products(
-    ID INT,
+    ID INT AUTO_INCREMENT,
     ProductName VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
 CREATE TABLE Warehouses(
-    ID INT,
+    ID INT AUTO_INCREMENT,
     WarehouseName VARCHAR(50),
     WarehouseAddress VARCHAR(100),
     CountryID INT,
@@ -25,7 +25,7 @@ CREATE TABLE Warehouses(
 );
 
 CREATE TABLE ProductInventory (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     ProductID INT,
     WarehouseID INT,
     WarehouseAmount INT,
@@ -36,27 +36,25 @@ CREATE TABLE ProductInventory (
 
 -- Populate test data
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
+INSERT INTO Countries (Name) VALUES ('Country1');
+INSERT INTO Countries (Name) VALUES ('Country2');
     
 
-INSERT INTO Products(ID, ProductName) VALUES (1, 'AwersomeProduct');
+INSERT INTO Products(ProductName) VALUES ('AwersomeProduct');
 
-INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
-VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
+INSERT INTO Warehouses (WarehouseName, WarehouseAddress, CountryID)
+VALUES ('Warehouse-1', 'City-1, Street-1', 1);
 
-INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount)
-VALUES (1, 1, 1, 2);
+INSERT INTO ProductInventory (ProductID, WarehouseID, WarehouseAmount)
+VALUES (1, 1, 2);
 
 
-INSERT INTO Products(ID, ProductName) VALUES (2, 'AwersomeProduct');
+INSERT INTO Products(ProductName) VALUES ('AwersomeProduct');
 
-INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID)
-VALUES (2, 'Warehouse-2',  'City-2, Street-2', 2);
+INSERT INTO Warehouses (WarehouseName, WarehouseAddress, CountryID)
+VALUES ('Warehouse-2',  'City-2, Street-2', 2);
 
-INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount)
-VALUES (2, 2, 2, 5);
+INSERT INTO ProductInventory (ProductID, WarehouseID, WarehouseAmount)
+VALUES (2, 2, 5);
 
 
